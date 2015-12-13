7ef1c7ce89ab9580bb046385640e509e545a60e7
bochaozhang <zhangbochao1222@gmail.com>
2015-12-13 04:17:13 -0500

    Add function to apply bias

--- countMutations.py
+++ countMutations.py
@@ -65,4 +65,59 @@ def divideMutation( unique_mutations,unique_count ):
             multiple_count = unique_count[unique_mutations.index(mutation)]
 
     return single_mutations, single_count, multiple_mutations, multiple_count 
-        
+       
+
+def groupMultipleMutation( multiple_mutations ):
+    "Group mutations from the same codon"
+    from math import ceil
+
+    multiple_group = [0]*len(multiple_mutations)
+
+    for i in range(len(multiple_mutations)):
+        if multiple_group[i]==0:
+            multiple_group[i] = i+1
+            for j in range(i+1,len(multiple_mutations)):
+                if multiple_group[j]==0:
+                    if multiple_mutations[i][0]==multiple_mutations[j][0] and multiple_mutations[i][2]==multiple_mutations[j][2] and ceil(float(multiple_mutations[i][1])/3)==ceil(float(multiple_mutations[j][1])/3):
+                        multiple_group[j] = i+1
+
+    return multiple_group
+
+
+def applyBias( multiple_mutations,multiple_count,multiple_group,bias ):
+    ""
+    from itertools import permutations
+    from Bio import Seq
+    from collections import Counter
+    
+    counted = [0]*len(multiple_mutations)
+    for mutation in multiple_mutations:
+        if counted[multiple_mutations.index(mutation)] != 1:
+            mismatch_positions = [i for i in range(len(mutation[0])) if mutation[0][i]!=mutation[2][i]]
+            p = list(permutations(mismatch_positions))
+            type_list = []
+            type_count = []
+            for i in range(len(p)):
+                types = []
+                germline = mutation[0]
+                for j in range(len(p[i])):
+                    mutated = germline[:p[i][j]] + mutation[2][p[i][j]] + germline[p[i][j]+1:]
+                    if 'N' not in germline and 'N' not in mutated:
+                        if Seq.translate(germline) == Seq.translate(mutated):
+                            types.append('silent')
+                        else:
+                            types.append('replacement')
+                    else:
+                        types.append('unkown')   
+                    germline = mutated
+                type_list.append(types)
+                type_frequency = Counter(types)
+                type_count.append(type_frequency[bias])
+            type_list = type_list[type_count.index(max(type_count))]
+                         
+            indices = [i for i, x in enumerate(multiple_group) if x == multiple_group[multiple_mutations.index(mutation)]]
+            for idx in indices:
+                counted[idx] = 1
+                multiple_mutations[idx].append(type_list[indices.index(idx)])
+    return multiple_mutations          
+            
