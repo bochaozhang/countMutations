bad3e7f74d41308dbc68c12d20e318b7045db0b2
bochaozhang <zhangbochao1222@gmail.com>
2015-12-18 05:26:24 -0500

    Removed some files...

--- run.py
+++
@@ -1,6 +0,0 @@
-import countMutations
-headerList,seqList = countMutations.readFasta(‘test.fasta’)
-unique_mutations,unique_count = countMutations.findMutations(seqList)
-single_mutations,single_count,multiple_mutations,multiple_count = countMutations.divideMutation(unique_mutations,unique_count)
-multiple_group = countMutations.groupMultipleMutation(multiple_mutations)
-multiple_mutations = countMutations.applyBias(multiple_mutations,multiple_count,multiple_group,bias)
